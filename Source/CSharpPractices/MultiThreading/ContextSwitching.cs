d902bdda13d8bd203418415fbd2ddad36b2eb6c5
Saeid Sabzalizadeh <saeid.sabzalizade@gmail.com>
2020-04-25 12:18:10 -0400

    ContextSwitching

---
+++ Source/CSharpPractices/MultiThreading/ContextSwitching.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    public class ContextSwitching
+    {
+        public static void Run()
+        {
+            Thread thread = new Thread(WriteUsingNewThread);
+            thread.Name = "Cazton Worker";
+            //Worker Thread
+            thread.Start();
+           
+            //Main Thread
+            Thread.CurrentThread.Name = "Cazton main";
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
+        }
+
+        private static void WriteUsingNewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z" + i + " ");
+            }
+        }
+
+
+    }
+}
---
+++ Source/CSharpPractices/Visualize/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Visualize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".................................");
+
+            MultiThreading.ContextSwitching.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
