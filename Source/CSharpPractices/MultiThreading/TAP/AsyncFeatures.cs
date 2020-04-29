fdbca736aa5dd591ead7f367a26e87e9cb0738e0
Saeid Sabzalizadeh <saeid.sabzalizade@gmail.com>
2020-04-29 17:15:25 -0400

    TAP

---
+++ Source/CSharpPractices/MultiThreading/TAP/AsyncFeatures.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.TAP
+{
+    public class AsyncFeatures
+    {
+        public static void Run()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                tokenSource.Cancel();
+            });
+
+
+            DownloadAsync(new Uri("https://jsonplaceholder.typicode.com/posts"), token).Wait();
+
+        }
+
+        public static async Task DownloadAsync(Uri uri, CancellationToken token)
+        {
+            var rand = new Random(258);
+
+            try
+            {
+
+                while (true)
+                {
+                    token.ThrowIfCancellationRequested();
+                    try
+                    {
+
+                        var uniqueNumber = rand.Next(100, 500);
+                        HttpResponseMessage result = await GetAsync(uri, uniqueNumber);
+
+                        Console.WriteLine($"{GetLogPrefix(uniqueNumber)}: After calling await for async method");
+                        Console.WriteLine($"{GetLogPrefix(uniqueNumber)}: After Xms of calling await for async method");
+                        Console.WriteLine($"{GetLogPrefix(uniqueNumber)}: Result is {result.StatusCode}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{GetLogPrefix()} - Exception: {ex.Message}");
+                    }
+                }
+
+            }
+            catch (OperationCanceledException pex)
+            {
+                Console.WriteLine($"{GetLogPrefix()}: Canceled by user.");
+            }
+
+        }
+
+        private static Task<HttpResponseMessage> GetAsync(Uri uri, int uniqueNumber)
+        {
+            Console.WriteLine($"{GetLogPrefix(uniqueNumber)}: I'm start of async Method");
+            Thread.Sleep(100);
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync(uri);
+            Console.WriteLine($"{GetLogPrefix(uniqueNumber)}: I'm end of async Method");
+            return result;
+        }
+
+        private static string GetLogPrefix(int? uniqueNumber = null)
+        {
+            return $"Thread[{(Thread.CurrentThread.ManagedThreadId > 9 ? $"{Thread.CurrentThread.ManagedThreadId}" : $"0{Thread.CurrentThread.ManagedThreadId}")}] - {DateTime.Now: mm:ss.fffff}{(uniqueNumber.HasValue ? $" (*{uniqueNumber})" : "")}";
+        }
+    }
+}
--- Source/CSharpPractices/Visualize/Program.cs
+++ Source/CSharpPractices/Visualize/Program.cs
@@ -8,6 +8,7 @@ namespace Visualize
         {
             Console.WriteLine(".................................");
 
+
             //MultiThreading.Threads.ContextSwitching.Run();
             //MultiThreading.Threads.SharedResource.Run();
             //MultiThreading.Threads.ThreadPoolFeatures.Run();
@@ -17,16 +18,19 @@ namespace Visualize
             //MultiThreading.Tasks.TaskFeatures.Run();
             //MultiThreading.Tasks.TaskIO.Run();
             //MultiThreading.Tasks.TaskContinuation.Run();
+
             //MultiThreading.Synchronization.Monitors.Run();
             //MultiThreading.Synchronization.DeadLocks.Run();
             //MultiThreading.Synchronization.ReaderWriterLocks.Run();
             //MultiThreading.Synchronization.ReaderWriterLocks.SecondWay.Run();
             //MultiThreading.Synchronization.MutexFeatures.Run();
             //MultiThreading.Synchronization.Semaphores.Run();
+
             //MultiThreading.Signaling.AutoResteEventFeatures.Run();
             //MultiThreading.Signaling.TwoWaySignaling.Run();
             //MultiThreading.Signaling.ManualResteEventFeatures.Run();
             //MultiThreading.Signaling.CountdownEventFeatures.Run();
+
             //MultiThreading.TPL.ParallelFor.Run();
             //MultiThreading.TPL.ParallelForeachModifyPictures.Run();
             //MultiThreading.TPL.Cancellation.Run();
@@ -35,13 +39,9 @@ namespace Visualize
 
             //MultiThreading.PLINQ.SimplePLINQ.Run();
             //MultiThreading.PLINQ.WithDegreeOfParallelism.Run();
-            MultiThreading.PLINQ.ConcurrencyBag.Run();
+            //MultiThreading.PLINQ.ConcurrencyBag.Run();
 
-
-
-
-
-
+            MultiThreading.TAP.AsyncFeatures.Run();
 
 
 
