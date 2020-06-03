742994eba0d6e2f2535101c8cf388a5ee558f59a
Saeid Sabzalizadeh <saeid.sabzalizade@gmail.com>
2020-06-03 14:16:16 -0400

    WeatherObservation try parse

--- Source/CSharpPractices/Challenges/CodeClinic/PondOreille/WeatherData.Tests/Tests.cs
+++ Source/CSharpPractices/Challenges/CodeClinic/PondOreille/WeatherData.Tests/Tests.cs
@@ -16,5 +16,18 @@ namespace WeatherData.Tests
             Check.That(wo.TimeStamp).IsEqualTo(new DateTime(2012, 01, 01, 00, 02, 14));
             Check.That(wo.Barometric_Pressure).IsCloseTo(30.5, 0.01);
         }
+
+        [Fact]
+        public void Test_020_TryParseLine()
+        {
+            var text = "2012_01_01 00:02:14	34.30	30.50	26.90	74.20	346.40	11.00	 3.60";
+
+            var tryParseOutcome = WeatherObservation.TryParse(text, out WeatherObservation wo);
+
+            Check.That(tryParseOutcome).IsTrue();
+            Check.That(wo.TimeStamp).IsEqualTo(new DateTime(2012, 01, 01, 00, 02, 14));
+            Check.That(wo.Barometric_Pressure).IsCloseTo(30.5, 0.01);
+        }
+
     }
 }
