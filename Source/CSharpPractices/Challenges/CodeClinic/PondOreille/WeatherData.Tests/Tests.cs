dd01bfb5bb4580a6bc4daacd8ffb6bd63d92f535
Saeid Sabzalizadeh <saeid.sabzalizade@gmail.com>
2020-06-03 13:49:16 -0400

    Add Weather Data

---
+++ Source/CSharpPractices/Challenges/CodeClinic/PondOreille/WeatherData.Tests/Tests.cs
@@ -0,0 +1,20 @@
+using NFluent;
+using System;
+using Xunit;
+
+namespace WeatherData.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test_010_ParseLine()
+        {
+            var text = "2012_01_01 00:02:14	34.30	30.50	26.90	74.20	346.40	11.00	 3.60";
+
+            var wo = WeatherObservation.Parse(text);
+
+            Check.That(wo.TimeStamp).IsEqualTo(new DateTime(2012, 01, 01, 00, 02, 14));
+            Check.That(wo.Barometric_Pressure).IsCloseTo(30.5, 0.01);
+        }
+    }
+}
