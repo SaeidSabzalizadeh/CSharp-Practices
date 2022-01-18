bb73f9ff4b3f1e441c232ea554c522b73c134e0a
Saeid Sabzalizadeh <saeid.sabzalizade@gmail.com>
2022-01-18 09:56:31 -0800

    add number 605

---
+++ Source/CSharpPractices/Challenges/LeetCode/Problems.LeetCode.Tests/All/N0605_CanPlaceFlowersTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.LeetCode.Easy;
+using Xunit;
+
+namespace Problems.LeetCode.Tests.All
+{
+    public class N0605_CanPlaceFlowersTests
+    {
+        [Theory]
+        [InlineData("[1,0,0,0,1]", 1, true)]
+        [InlineData("[1,0,0,0,1]", 2, false)]
+        [InlineData("[1,0,0,0,0,0,1]", 2, true)]
+        [InlineData("[0,0,1,0,0,0,1]", 2, true)]
+        [InlineData("[1,0,0,0,1,0,0]", 2, true)]
+        [InlineData("[0]", 1, true)]
+        public void TestSolution1(string flowerbedArrayStr, int n, bool expectedResult)
+        {
+
+            int[] flowerbed = Helper.GetIntArray(flowerbedArrayStr);
+            bool result = N0605_CanPlaceFlowers.Solution(flowerbed, n);
+
+            Assert.True(result == expectedResult, $"result is not as expected. Actual: {result} - Expected: {expectedResult}");
+
+        }
+
+    }
+}
---
+++ Source/CSharpPractices/Challenges/LeetCode/Problems.LeetCode/Easy/N0605_CanPlaceFlowers.cs
@@ -0,0 +1,78 @@
+﻿namespace Problems.LeetCode.Easy
+{
+
+
+    /// <summary>
+    /// 
+    /// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+    /// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+    /// and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+    /// 
+    /// Example 1:
+    /// Input: flowerbed = [1,0,0,0,1], n = 1
+    /// Output: true
+    /// 
+    /// Example 2:
+    /// Input: flowerbed = [1,0,0,0,1], n = 2
+    /// Output: false
+    /// 
+    /// Constraints:
+    /// 1 <= flowerbed.length <= 2 * 10^4
+    /// flowerbed[i] is 0 or 1
+    /// There are no two adjacent flowers in flowerbed
+    /// 0 <= n <= flowerbed.length
+    /// 
+    /// 
+    /// https://leetcode.com/problems/can-place-flowers/
+    /// </summary>
+    public class N0605_CanPlaceFlowers
+    {
+
+        public static bool Solution(int[] flowerbed, int n)
+        {
+            if (n <= 0)
+                return true;
+
+            if (flowerbed.Length == 1)
+            {
+                return flowerbed[0] == 0;
+            }
+
+            int numOfContinuesZero = 0;
+
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0)
+                    numOfContinuesZero++;
+                else
+                    numOfContinuesZero = 0;
+
+                if (numOfContinuesZero == 3)
+                {
+                    numOfContinuesZero = 1;
+                    n--;
+                }
+
+                if (i == 1 && numOfContinuesZero == 2)
+                {
+                    numOfContinuesZero = 1;
+                    n--;
+                }
+
+                if (i == flowerbed.Length - 1 && numOfContinuesZero == 2)
+                {
+                    numOfContinuesZero = 1;
+                    n--;
+                }
+
+
+                if (n == 0)
+                    return true;
+
+            }
+
+            return false;
+        }
+
+    }
+}
